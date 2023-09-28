943973375abb5cc9c61cf8a84c9ecb696bd73cb8
Leandro <leango259@gmail.com>
2023-09-28 13:14:31 -0300

    subiendo repo

---
+++ js/lightbox.js
@@ -0,0 +1,4 @@
+const imagenes = document.querySelectorAll(".img-galeria")
+const imageneslight = document.querySelector(".agregar-imagen")
+const contenedorlight = document.querySelector(".imagen-light")
+
